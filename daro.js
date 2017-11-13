684512f11c2b88dfe9ce4f5fac9b2dd540ce88c8
LaSoeur <33630329+LaSoeur@users.noreply.github.com>
2017-11-13 16:12:28 +0100

    Update daro.js

--- daro.js
+++ daro.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Daro GIT
-// @namespace    http://tampermonkey.net/
+// @namespace    lasoeur.fr
 // @version      0.1
 // @description  try to take over the world!
 // @author       LaSoeur
